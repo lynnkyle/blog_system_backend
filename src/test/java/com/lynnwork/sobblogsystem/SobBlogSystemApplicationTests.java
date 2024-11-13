d8634929f82b15f1a0f93243da80e39cf3719ce1
LinZeyuan <903586678@qq.com>
2024-11-13 22:29:54 +0800

    submit_version_4

--- src/test/java/com/lynnwork/sobblogsystem/SobBlogSystemApplicationTests.java
+++ src/test/java/com/lynnwork/sobblogsystem/SobBlogSystemApplicationTests.java
@@ -1,27 +1,45 @@
 package com.lynnwork.sobblogsystem;
 
 import com.lynnwork.sobblogsystem.mapper.DemoMapper;
-import com.lynnwork.sobblogsystem.pojo.Demo;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
 
 @Slf4j
 @SpringBootTest
 class SobBlogSystemApplicationTests {
     @Autowired
     private DemoMapper demoMapper;
+    @Autowired
+    private JavaMailSender mailSender;
 
     @Test
-    public void mapperTest() {
-        demoMapper.insert(new Demo("张三", "12346"));
-        demoMapper.insert(new Demo("李四", "12346"));
-        demoMapper.insert(new Demo("王五", "123456"));
-        demoMapper.insert(new Demo("李六", "123456"));
-        demoMapper.insert(new Demo("梅洋", "123456"));
-        demoMapper.insert(new Demo("鹤滨", "123456"));
-        demoMapper.insert(new Demo("皓哲", "123456"));
+    public void mapperTest() throws Exception{
+        MimeMessage mimeMessage= mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,true);
+        //1.设置邮件的发送人、收件人、主题
+        helper.setFrom("13788809958@163.com","阳光沙滩博客系统");
+        helper.setTo("903586678@qq.com");
+        helper.setSubject("The Email with HTML content and Attachment");
+        //2.设置邮件的文本内容
+        String htmlText = "<h1>Hello, this is a test email</h1>" +
+                "<p>This is a <b>HTML</b> content example!</p>" +
+                "<p>Here is an <a href='https://www.example.com'>example link</a></p>";
+        helper.setText(htmlText,true);
+        //3.设置附件
+        File file1=new File("src/main/resources/static/default_avatar.png");
+        File file2=new File("src/main/resources/static/index.html");
+        helper.addAttachment("avatar.png", file1);
+        helper.addAttachment("avatar.html", file2);
+        //4.发送邮件
+        mailSender.send(mimeMessage);
     }
-
 }
